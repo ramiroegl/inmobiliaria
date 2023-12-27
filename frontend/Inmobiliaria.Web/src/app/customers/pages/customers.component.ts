cce0c11788191de6cc33ee1c58a5f23b2bc35643
Ramiro González <ramiro.gl@hotmail.com>
2023-12-26 19:19:38 -0500

    Agregar página de login

--- frontend/Inmobiliaria.Web/src/app/customers/pages/customers.component.ts
+++ frontend/Inmobiliaria.Web/src/app/customers/pages/customers.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Customer } from '../../shared/models/customer';
 import { CustomerItemComponent } from '../components/customer-item/customer-item.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [CustomerItemComponent],
+  imports: [CustomerItemComponent, RouterLink],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
@@ -18,5 +19,68 @@ export class CustomersComponent {
     lastnames: 'Gonzalez',
     names: 'Ramiro',
     salary: 1200000
+  },
+  {
+    id: '2',
+    cellphone: '3432432',
+    civilStatus: 'casado',
+    identification: '3243242',
+    lastnames: 'Martinez',
+    names: 'Juan',
+    salary: 1450000
+  },
+  {
+    id: '1',
+    cellphone: '213123',
+    civilStatus: 'soltero',
+    identification: '10231232',
+    lastnames: 'Gonzalez',
+    names: 'Ramiro',
+    salary: 1200000
+  },
+  {
+    id: '2',
+    cellphone: '3432432',
+    civilStatus: 'casado',
+    identification: '3243242',
+    lastnames: 'Martinez',
+    names: 'Juan',
+    salary: 1450000
+  },
+  {
+    id: '1',
+    cellphone: '213123',
+    civilStatus: 'soltero',
+    identification: '10231232',
+    lastnames: 'Gonzalez',
+    names: 'Ramiro',
+    salary: 1200000
+  },
+  {
+    id: '2',
+    cellphone: '3432432',
+    civilStatus: 'casado',
+    identification: '3243242',
+    lastnames: 'Martinez',
+    names: 'Juan',
+    salary: 1450000
+  },
+  {
+    id: '1',
+    cellphone: '213123',
+    civilStatus: 'soltero',
+    identification: '10231232',
+    lastnames: 'Gonzalez',
+    names: 'Ramiro',
+    salary: 1200000
+  },
+  {
+    id: '2',
+    cellphone: '3432432',
+    civilStatus: 'casado',
+    identification: '3243242',
+    lastnames: 'Martinez',
+    names: 'Juan',
+    salary: 1450000
   }];
 }
