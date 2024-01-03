079657f1dd0cfad718a20a3a42b87038fb6d2f25
Ramiro González <ramiro.gl@hotmail.com>
2024-01-03 18:43:47 -0500

    Se agrega modal al registrar nuevo cliente

--- frontend/Inmobiliaria.Web/src/app/app.routes.ts
+++ frontend/Inmobiliaria.Web/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './shared/pages/home/home.component';
 import { UsersComponent } from './users/users.component';
 import { LoginComponent } from './login/login.component';
 import { NotfoundComponent } from './shared/pages/notfound/notfound.component';
+import { NewCustomerComponent } from './customers/pages/new-customer/new-customer.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'customers', component: CustomersComponent },
+    { path: 'customers/new', component: NewCustomerComponent },
     { path: 'login', component: LoginComponent },
     { path: 'sales', component: SalesComponent },
     { path: 'users', component: UsersComponent },
