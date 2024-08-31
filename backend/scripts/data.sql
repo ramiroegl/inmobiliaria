724877162d5648cb0f31494f61878621460104f1
ramiroegl <133180691+ramiroegl@users.noreply.github.com>
2024-08-31 09:13:46 -0500

    Create data.sql

---
+++ backend/scripts/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO public."User" ("Id", "Name", "LastName", "Email", "Password", "CreatedOn", "UpdatedOn", "Role")
+VALUES ('941dd6cd-c3fb-44fd-a415-303afbab587f', 'Admin', 'Admin', 'admin@admin.com', 'tjdbTClH7iMrZ/hEPR1Ztg==;JbBqDU/Ljm88Rosn7fKWEpRGRpJz/kBD3n+p7eI8exw=', now(), now(), 'Admin');
